578073d5b50a7b154c15aab782ef51344baf3f6e
Pablo Manuel Fernández Velazquez <163730673+ProfitCoder@users.noreply.github.com>
2025-11-27 10:31:52 +0100

    Add files via upload

---
+++ Unidad 1/PracticaSuma.java
@@ -0,0 +1,15 @@
+public class PracticaSuma
+{
+	public static void main(String [] args)
+	{
+		int a=10, b=5;
+		b+=a;
+		System.out.printf("a=%d y b=%d",a,b);
+	}
+}
+
+
+
+/*Lo que pasa aqui es que el %d sirve como cadena para coger el valor de despues
+y que el += lo que hace es sumar*/
+
