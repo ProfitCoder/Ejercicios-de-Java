08f0ae42d80bae7022a513aa81ec576d93091408
Pablo Manuel Fernández Velazquez <163730673+ProfitCoder@users.noreply.github.com>
2025-11-27 10:36:34 +0100

    Add files via upload

---
+++ Unidad 1/PruebaDouble.java
@@ -0,0 +1,16 @@
+public class PruebaDouble{
+	public static void main(String [] args)
+	{
+		Double a,b,diferencia;
+		a=24.4;
+		b=23.0;
+		
+		//Para mostrar la diferencia
+		diferencia = a - b;
+		System.out.println(diferencia);
+	}
+}
+
+
+
+//Sale de solucion 1.399999999986 porque el ordenador no puede guardar 1.4 y los 23 o 24.4 tampoco
