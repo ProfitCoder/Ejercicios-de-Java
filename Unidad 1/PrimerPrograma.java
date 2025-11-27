6eb535a0e1a8f8d0c96087ed777b325019ee9303
Pablo Manuel Fernández Velazquez <163730673+ProfitCoder@users.noreply.github.com>
2025-11-27 10:33:54 +0100

    Add files via upload

---
+++ Unidad 1/PrimerPrograma.java
@@ -0,0 +1,12 @@
+public class PrimerPrograma
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Módulo Profesional - PROGRAMACIÓN");
+		System.out.println("UNIDAD 1 - Introducción a la programación");
+		System.out.println("Pablo Manuel Fernández Velázquez");
+		System.out.println("22/09/2025");
+		System.out.println("Este es mi primer programa escrito en Java");
+	}
+}
+	
