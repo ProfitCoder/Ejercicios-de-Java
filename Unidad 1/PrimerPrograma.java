56f6866ed6294e2744560a172fd61ecfb1c0d71e
Pablo Manuel Fernández Velazquez <163730673+ProfitCoder@users.noreply.github.com>
2025-11-27 10:35:06 +0100

    Update PrimerPrograma.java

--- Unidad 1/PrimerPrograma.java
+++ Unidad 1/PrimerPrograma.java
@@ -1,12 +1,12 @@
 public class PrimerPrograma
-{
 	public static void main(String[] args)
 	{
-		System.out.println("Módulo Profesional - PROGRAMACIÓN");
+		System.out.println("Mi primer programa");
 		System.out.println("UNIDAD 1 - Introducción a la programación");
-		System.out.println("Pablo Manuel Fernández Velázquez");
+		System.out.println("Created by ProfitCoder");
 		System.out.println("22/09/2025");
 		System.out.println("Este es mi primer programa escrito en Java");
 	}
 }
+
 	
